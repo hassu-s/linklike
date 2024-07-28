5ecd46c5f1209f12570a129698cef1b9dfe139d2
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-29 01:47:06 +0900

    Ver:0.4.02

--- javascript/IE.js
+++ javascript/IE.js
@@ -1,6 +1,30 @@
-if (window.location.pathname === '/IE.html') {
-    if (window.navigator.userAgent.indexOf('MSIE') === -1 && !document.documentMode) {
-        // Not Internet Explorer detected on IE.html page
-        window.location.href = '404.html';
+(function() {
+    // IEブラウザを検出する関数
+    function isIE() {
+        var ua = window.navigator.userAgent;
+        var msie = ua.indexOf('MSIE ') > -1;
+        var trident = ua.indexOf('Trident/') > -1;
+        return msie || trident;
     }
-}
+
+    // 現在のページがIE.htmlかどうかを確認する関数
+    function isIEPage() {
+        return window.location.pathname.endsWith('IE.html');
+    }
+
+    // 現在のページが404.htmlかどうかを確認する関数
+    function is404Page() {
+        return window.location.pathname.endsWith('404.html');
+    }
+
+    // メインのロジック
+    if (isIE()) {
+        if (!isIEPage()) {
+            window.location.href = 'IE.html';
+        }
+    } else {
+        if (isIEPage()) {
+            window.location.href = '404.html';
+        }
+    }
+})();
