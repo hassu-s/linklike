4ea82fcf7bdecb6657bab35df13ccfe333e26494
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-29 01:34:10 +0900

    Ver:0.4.0

--- javascript/IE.js
+++ javascript/IE.js
@@ -1,4 +1,6 @@
-if (window.navigator.userAgent.indexOf('MSIE') !== -1 || !!document.documentMode) {
-    // Internet Explorer detected
-    window.location.href = 'IE.html';
+if (window.location.pathname === '/IE.html') {
+    if (window.navigator.userAgent.indexOf('MSIE') === -1 && !document.documentMode) {
+        // Not Internet Explorer detected on IE.html page
+        window.location.href = '404.html';
+    }
 }
