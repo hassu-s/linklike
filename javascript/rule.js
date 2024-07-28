f0f3d4f052ad2ab0a0b2b2364eaebf841f57ed90
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-28 23:34:09 +0900

    Ver:0.3.6

---
+++ javascript/rule.js
@@ -0,0 +1,16 @@
+// 右クリックを禁止
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+// 文字選択を禁止
+document.addEventListener('selectstart', function(e) {
+    e.preventDefault();
+});
+
+// サイトの拡大を禁止
+document.addEventListener('wheel', function(e) {
+    if (e.ctrlKey) {
+        e.preventDefault();
+    }
+}, { passive: false });
