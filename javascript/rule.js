df0b7db55acbd7b4862b9315b8e4e29a8d1373ec
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-29 02:21:44 +0900

    Ver:0.4.1

--- javascript/rule.js
+++ javascript/rule.js
@@ -14,3 +14,22 @@ document.addEventListener('wheel', function(e) {
         e.preventDefault();
     }
 }, { passive: false });
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+
+        // 日本の緯度経度範囲
+        var japanLatMin = 24.396308;
+        var japanLatMax = 45.551483;
+        var japanLonMin = 122.93457;
+        var japanLonMax = 153.986672;
+
+        if (lat < japanLatMin || lat > japanLatMax || lon < japanLonMin || lon > japanLonMax) {
+            window.location.href = "404.html";
+        }
+    });
+} else {
+    window.location.href = "404.html";
+}
