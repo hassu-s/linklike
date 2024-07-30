6a7052b28413da82b23df2e8e45d8ce3085e29a8
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-30 16:48:16 +0900

    Ver:0.6.15

--- javascript/maintenance.js
+++ javascript/maintenance.js
@@ -1,6 +1,6 @@
 (function() {
-    var maintenanceStart = new Date('2024-07-30T16:47:00+09:00'); // 日本時間で設定
-    var maintenanceEnd = new Date('2024-07-30T16:49:00+09:00'); // 日本時間で設定
+    var maintenanceStart = new Date('2024-07-30T16:51:00+09:00'); // 日本時間で設定
+    var maintenanceEnd = new Date('2024-07-30T16:53:00+09:00'); // 日本時間で設定
     var currentTime = new Date();
 
     if (currentTime >= maintenanceStart && currentTime <= maintenanceEnd) {
