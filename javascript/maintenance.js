76a5070e70785f2657c726064e0d7ed64572e5de
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-30 16:54:24 +0900

    Ver:0.6.17

--- javascript/maintenance.js
+++ javascript/maintenance.js
@@ -1,11 +1,14 @@
 (function() {
-    var maintenanceStart = new Date('2024-07-30T16:51:00+09:00'); // 日本時間で設定
-    var maintenanceEnd = new Date('2024-07-30T16:53:00+09:00'); // 日本時間で設定
+    var maintenanceStart = new Date('2024-07-30T16:58:00+09:00'); // 日本時間で設定
+    var maintenanceEnd = new Date('2024-07-30T17:00:00+09:00'); // 日本時間で設定
     var currentTime = new Date();
+    var isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-    if (currentTime >= maintenanceStart && currentTime <= maintenanceEnd) {
-        window.location.href = '../html/maintenance.html';
-    } else {
-        window.location.href = '../linklike/404.html';
+    if (!isLocal) {
+        if (window.location.pathname === '../linklike/html/maintenance.html') {
+            if (currentTime < maintenanceStart || currentTime > maintenanceEnd) {
+                window.location.href = '../404.html';
+            }
+        }
     }
 })();
