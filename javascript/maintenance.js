e9716ef283b470ae534f4ed0c3b80a76d8a823d2
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-30 17:02:46 +0900

    Ver:0.6.18

--- javascript/maintenance.js
+++ javascript/maintenance.js
@@ -1,14 +1,19 @@
 (function() {
-    var maintenanceStart = new Date('2024-07-30T16:58:00+09:00'); // 日本時間で設定
-    var maintenanceEnd = new Date('2024-07-30T17:00:00+09:00'); // 日本時間で設定
-    var currentTime = new Date();
-    var isLocal = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+    // リダイレクト先のURL
+    const maintenanceUrl = "https://hassu-s.github.io/linklike/html/maintenance";
+    const originalUrl = "https://hassu-s.github.io/linklike/";
 
-    if (!isLocal) {
-        if (window.location.pathname === '../linklike/html/maintenance.html') {
-            if (currentTime < maintenanceStart || currentTime > maintenanceEnd) {
-                window.location.href = '../404.html';
-            }
-        }
+    // 現在の日時を取得
+    const now = new Date();
+
+    // リダイレクトする開始時間と終了時間を設定
+    const startTime = new Date(2024, 6, 30, 17, 6); // 2024年7月30日17時6分
+    const endTime = new Date(2024, 6, 30, 17, 8); // 2024年7月30日17時8分
+
+    // 現在の時間が指定された時間帯に含まれているかをチェック
+    if (now >= startTime && now <= endTime) {
+        window.location.href = maintenanceUrl;
+    } else if (now > endTime) {
+        window.location.href = originalUrl;
     }
 })();
