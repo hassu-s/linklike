6472e036bd7c44a8810cf378582b18ffcaad73a2
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-30 17:10:38 +0900

    Ver:0.6.182

--- javascript/maintenance.js
+++ javascript/maintenance.js
@@ -1,19 +1,22 @@
 (function() {
-    // リダイレクト先のURL
-    const maintenanceUrl = "https://hassu-s.github.io/linklike/html/maintenance";
+    const maintenanceUrl = "https://hassu-s.github.io/linklike/html/maintenance?redirected=true";
     const originalUrl = "https://hassu-s.github.io/linklike/";
 
-    // 現在の日時を取得
-    const now = new Date();
+    const startTime = new Date(2024, 6, 30, 17, 13); // 2024年7月30日17時13分
+    const endTime = new Date(2024, 6, 30, 17, 14); // 2024年7月30日17時14分
 
-    // リダイレクトする開始時間と終了時間を設定
-    const startTime = new Date(2024, 6, 30, 17, 6); // 2024年7月30日17時6分
-    const endTime = new Date(2024, 6, 30, 17, 8); // 2024年7月30日17時8分
+    function checkTime() {
+        const now = new Date();
+        const urlParams = new URLSearchParams(window.location.search);
+        const redirected = urlParams.get('redirected');
 
-    // 現在の時間が指定された時間帯に含まれているかをチェック
-    if (now >= startTime && now <= endTime) {
-        window.location.href = maintenanceUrl;
-    } else if (now > endTime) {
-        window.location.href = originalUrl;
+        if (now >= startTime && now <= endTime && !redirected) {
+            window.location.href = maintenanceUrl;
+        } else if (now > endTime && redirected) {
+            window.location.href = originalUrl;
+        }
     }
+
+    // 1秒ごとに時間をチェック
+    setInterval(checkTime, 1000);
 })();
