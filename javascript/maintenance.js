0f16c7c9e1ab023c0337e5a2af013aaa42ded0b3
はっすーず <hassus.linklikelovelive@gmail.com>
2024-07-30 17:24:39 +0900

    Ver:0.6.2

--- javascript/maintenance.js
+++ javascript/maintenance.js
@@ -1,9 +1,9 @@
 (function() {
     const maintenanceUrl = "https://hassu-s.github.io/linklike/html/maintenance?redirected=true";
-    const originalUrl = "https://hassu-s.github.io/linklike/";
+    const originalUrl = "https://hassu-s.github.io/linklike/?redirected=true";
 
-    const startTime = new Date(2024, 6, 30, 17, 19); // 2024年7月30日17時19分
-    const endTime = new Date(2024, 6, 30, 17, 20); // 2024年7月30日17時20分
+    const startTime = new Date(2024, 6, 30, 17, 27); // 2024年7月30日17時6分
+    const endTime = new Date(2024, 6, 30, 17, 28); // 2024年7月30日17時8分
 
     function checkTime() {
         const now = new Date();
@@ -12,12 +12,8 @@
 
         if (now >= startTime && now <= endTime && !redirected) {
             window.location.href = maintenanceUrl;
-        } else if (now > endTime) {
-            if (redirected) {
-                window.location.href = originalUrl;
-            } else {
-                window.location.href = originalUrl + "?redirected=true";
-            }
+        } else if (now > endTime && !redirected) {
+            window.location.href = originalUrl;
         }
     }
 
