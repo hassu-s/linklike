ebb1c46e01e48be90e4e722d9e80b7496126b374
はっすーず <hassus.linklikelovelive@gmail.com>
2024-08-05 14:01:28 +0900

    Ver:0.6.505

--- javascript/error.js
+++ javascript/error.js
@@ -1,5 +1,19 @@
 window.addEventListener('error', function(event) {
     if (event.target.tagName === 'IMG' || event.target.tagName === 'SCRIPT' || event.target.tagName === 'LINK') {
+        const url = event.target.src || event.target.href;
+        if (url) {
+            const extension = url.split('.').pop();
+            if (extension === 'css' || extension === 'js' || extension === 'csv') {
+                window.location.href = '/403.html';
+                return;
+            }
+        }
         window.location.href = '/404.html';
     }
 }, true);
+
+window.addEventListener('load', function() {
+    if (window.location.pathname === '/414.html') {
+        window.location.href = '/414.html';
+    }
+});
