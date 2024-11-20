b377a712eeed70f5ae9492746c6003cf0fb5cd92
はっすーず <hassus.linklikelovelive@gmail.com>
2024-11-20 09:01:53 +0900

    Ver:0.0.029

--- javascript/error.js
+++ javascript/error.js
@@ -3,3 +3,14 @@ window.addEventListener('error', function(event) {
         window.location.href = '/404.html';
     }
 }, true);
+
+window.addEventListener('error', function(event) {
+    const restrictedExtensions = ['.js', '.css', '.txt', '.csv'];
+    
+    if (event.target.tagName === 'IMG' || event.target.tagName === 'SCRIPT' || event.target.tagName === 'LINK') {
+        const src = event.target.src || event.target.href;
+        if (src && restrictedExtensions.some(ext => src.endsWith(ext))) {
+            window.location.href = '/403.html';
+        }
+    }
+}, true);
