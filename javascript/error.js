2e3d98c8548c30e5a6ee7c564fe4f8fd7b06146b
はっすーず <hassus.linklikelovelive@gmail.com>
2024-11-20 09:17:33 +0900

    Ver:0.0.033

--- javascript/error.js
+++ javascript/error.js
@@ -4,15 +4,3 @@ window.addEventListener('error', function(event) {
     }
 }, true);
 
-document.addEventListener('DOMContentLoaded', function () {
-    // 必須の<meta>タグを定義
-    const requiredMetaName = 'access-allowed'; // 必須のmetaタグの名前
-    const requiredMeta = document.querySelector(`meta[name="${requiredMetaName}"]`);
-
-    // <meta>タグが存在しない場合は403ページにリダイレクト
-    if (!requiredMeta) {
-        console.warn('Required <meta> tag is missing. Redirecting to /403.html');
-        window.location.href = '/403.html';
-    }
-});
-
